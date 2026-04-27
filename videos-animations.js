74d85ccf8e6a874d4e6018971dce2f5924ec7d7a
valknuthands-blip <valknuthands@gmail.com>
2026-04-28 10:37:51 +1200

    Add files via upload

---
+++ videos-animations.js
@@ -0,0 +1,13 @@
+// (c) 2026 Vivash Singh - All Rights Reserved
+// (c) 2026 Vivash Singh - All Rights Reserved
+// To update: replace YouTube ID. type: "short" = portrait, "landscape" = widescreen
+var ANIMATION_VIDEOS = [
+    { id: "wqCc4geO5DI", type: "short"     },
+    { id: "MWThSXPTc_c", type: "short"     },
+    { id: "zv0xI634rqs", type: "short"     },
+    { id: "hZNuPDXFEpc", type: "landscape" },
+    { id: "kzaSZNhqbhA", type: "landscape" },
+    { id: "ul-0FkNP1ws", type: "short"     },
+    { id: "GjzEislSLNo", type: "landscape" },
+    { id: "2V0g2MkoTVw", type: "landscape" }
+];
---
+++ videos-webdev.js
@@ -0,0 +1,9 @@
+// (c) 2026 Vivash Singh - All Rights Reserved
+// (c) 2026 Vivash Singh - All Rights Reserved
+// To update: replace the YouTube ID in the "id" field only
+var WEBDEV_VIDEOS = [
+    { id: "nqnIC97hcJg" },
+    { id: "JyUP5vBN1hk" },
+    { id: "eHui1R48kTo" },
+    { id: "c4hrjQiM080" }
+];
