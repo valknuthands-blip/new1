f66c66e32e458f382a955dcde484a904c2a8a650
valknuthands-blip <valknuthands@gmail.com>
2026-05-28 09:31:53 +1200

    Delete videos-webdev.js

--- videos-webdev.js
+++
@@ -1,9 +0,0 @@
-// (c) 2026 Vivash Singh - All Rights Reserved
-// (c) 2026 Vivash Singh - All Rights Reserved
-// To update: replace the YouTube ID in the "id" field only
-var WEBDEV_VIDEOS = [
-    { id: "nqnIC97hcJg" },
-    { id: "JyUP5vBN1hk" },
-    { id: "eHui1R48kTo" },
-    { id: "c4hrjQiM080" }
-];
